f0be0d17ec620de7c3e63b544ec8605389aa63b0
Evel <evenezuela28@gmail.com>
2018-12-25 19:33:13 -0400

    2 Se agrega el botón Actualizar Usuario

--- Usuario.py
+++ Usuario.py
@@ -21,10 +21,19 @@ class usuario:
         cone.commit()
         cone.close()
 
+    def Actualizar(self, datos):
+        cone=self.abrir()
+        cursor=cone.cursor()
+        sql="update registrar set Nombre = %s, Apellido = %s, Edad = %s where Cedula = %s"
+        cursor.execute(sql, datos)
+        cone.commit()
+        cone.close()
+        return cursor.rowcount # retornamos la cantidad de filas modificadas
+
     def consultar(self, datos):
         cone=self.abrir()
         cursor=cone.cursor()
-        sql="select Nombre, Apellido, Cedula, Edad from registrar where Cedula = %s"
+        sql="select Nombre, Apellido, Cedula, Edad from registrar where Cedula = %s" # Corrección en edad por ortografía.
         cursor.execute(sql, datos)
         cone.close()
         return cursor.fetchall()   
@@ -32,7 +41,7 @@ class usuario:
     def ListaDeDatos(self):
         cone=self.abrir()
         cursor=cone.cursor()
-        sql="select Cedula, Nombre, Apellido, Edada from registrar"
+        sql="select Cedula, Nombre, Apellido, Edad from registrar" 
         cursor.execute(sql)
         cone.close()
         return cursor.fetchall()
--- VentanaUsuario.py
+++ VentanaUsuario.py
@@ -13,6 +13,7 @@ class Aplicacion:
         self.cuaderno1 = ttk.Notebook(self.ventana1)  
         self.registrarUsuario()
         self.ConsultarUsuario()
+        self.ActualizarUsuario()
         self.cuaderno1.grid(column=0, row=0, padx=10, pady=10)
 
         self.ventana1.mainloop()
@@ -30,20 +31,20 @@ class Aplicacion:
         self.entryCedula.grid(column=1, row=0, padx=4, pady=4)# se coloca la ubicacion del Entry
         #
         self.Nombre = tk.StringVar()# se declara la variable
-        self.label1=ttk.Label(self.labelframe1, text="Nombre:")
-        self.label1.grid(column=0, row=1, padx=4, pady=4)
+        self.label3=ttk.Label(self.labelframe1, text="Nombre:")
+        self.label3.grid(column=0, row=1, padx=4, pady=4)
         self.entryNombre=ttk.Entry(self.labelframe1, textvariable=self.Nombre)
         self.entryNombre.grid(column=1, row=1, padx=4, pady=4)
         #
         self.Apellido = tk.StringVar()# se declara la variable
-        self.label2=ttk.Label(self.labelframe1, text="Apellido:")        
-        self.label2.grid(column=0, row=2, padx=4, pady=4)
+        self.label4=ttk.Label(self.labelframe1, text="Apellido:")        
+        self.label4.grid(column=0, row=2, padx=4, pady=4)
         self.entryApellido=ttk.Entry(self.labelframe1, textvariable=self.Apellido)
         self.entryApellido.grid(column=1, row=2, padx=4, pady=4)
         #
         self.Edad = tk.StringVar()# se declara la variable
-        self.label2=ttk.Label(self.labelframe1, text="Edad:")        
-        self.label2.grid(column=0, row=3, padx=4, pady=4)
+        self.label5=ttk.Label(self.labelframe1, text="Edad:")        
+        self.label5.grid(column=0, row=3, padx=4, pady=4)
         self.entryEdad=ttk.Entry(self.labelframe1, textvariable=self.Edad)
         self.entryEdad.grid(column=1, row=3, padx=4, pady=4)
         #
@@ -52,7 +53,7 @@ class Aplicacion:
 
     def ConsultarUsuario(self):
         self.pagina2 = ttk.Frame(self.cuaderno1)
-        self.cuaderno1.add(self.pagina2, text="Consulta Usuario")
+        self.cuaderno1.add(self.pagina2, text="Consulta Usuario")   
         self.labelframe1=ttk.LabelFrame(self.pagina2, text="Consulta")
         self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
         #
@@ -63,20 +64,20 @@ class Aplicacion:
         self.entryCedula.grid(column=1, row=0, padx=4, pady=4)
         #
         self.Nombre = tk.StringVar()# se declara la variable
-        self.label1=ttk.Label(self.labelframe1, text="Nombre:")
-        self.label1.grid(column=0, row=1, padx=4, pady=4)
+        self.label3=ttk.Label(self.labelframe1, text="Nombre:")
+        self.label3.grid(column=0, row=1, padx=4, pady=4)
         self.entryNombre=ttk.Entry(self.labelframe1, textvariable=self.Nombre,state="readonly")
         self.entryNombre.grid(column=1, row=1, padx=4, pady=4)
         #
         self.Apellido = tk.StringVar()# se declara la variable
-        self.label2=ttk.Label(self.labelframe1, text="Apellido:")        
-        self.label2.grid(column=0, row=2, padx=4, pady=4)
+        self.label4=ttk.Label(self.labelframe1, text="Apellido:")        
+        self.label4.grid(column=0, row=2, padx=4, pady=4)
         self.entryApellido=ttk.Entry(self.labelframe1, textvariable=self.Apellido, state="readonly")
         self.entryApellido.grid(column=1, row=2, padx=4, pady=4)
         #
         self.Edad = tk.StringVar()# se declara la variable
-        self.label2=ttk.Label(self.labelframe1, text="Edad:")        
-        self.label2.grid(column=0, row=3, padx=4, pady=4)
+        self.label5=ttk.Label(self.labelframe1, text="Edad:")        
+        self.label5.grid(column=0, row=3, padx=4, pady=4)
         self.entryEdad=ttk.Entry(self.labelframe1, textvariable=self.Edad, state="readonly")
         self.entryEdad.grid(column=1, row=3, padx=4, pady=4)
         #
@@ -100,5 +101,52 @@ class Aplicacion:
                 self.Edad.set(respuesta[0][3])
             else:
                 mb.showinfo("Información", "No existe el numero de cedula.")
+
+        
+    def ActualizarUsuario(self):        
+        self.pagina3 = ttk.Frame(self.cuaderno1)
+        self.cuaderno1.add(self.pagina3, text="Actualizar Usuario")
+        self.labelframe1=ttk.LabelFrame(self.pagina3, text="Usuario")        
+        self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
+        # 
+        self.Cedula = tk.StringVar()# se declara la variable
+        self.label2=ttk.Label(self.labelframe1, text="Cedula:")# se crea el control Label para la etiqueta     
+        self.label2.grid(column=0, row=0, padx=4, pady=4)# se coloca la ubicacion del Label
+        self.entryCedula=ttk.Entry(self.labelframe1, textvariable=self.Cedula)# se crea el control Entry     
+        self.entryCedula.grid(column=1, row=0, padx=4, pady=4)# se coloca la ubicacion del Entry
+
+        self.Nombre = tk.StringVar()# se declara la variable
+        self.label3=ttk.Label(self.labelframe1, text="Nombre:")
+        self.label3.grid(column=0, row=1, padx=4, pady=4)
+        self.entryNombre=ttk.Entry(self.labelframe1, textvariable=self.Nombre)
+        self.entryNombre.grid(column=1, row=1, padx=4, pady=4)
+        #
+        self.Apellido = tk.StringVar()# se declara la variable
+        self.label4=ttk.Label(self.labelframe1, text="Apellido:")        
+        self.label4.grid(column=0, row=2, padx=4, pady=4)
+        self.entryApellido=ttk.Entry(self.labelframe1, textvariable=self.Apellido)
+        self.entryApellido.grid(column=1, row=2, padx=4, pady=4)
+        #
+        self.Edad = tk.StringVar()# se declara la variable
+        self.label5=ttk.Label(self.labelframe1, text="Edad:")        
+        self.label5.grid(column=0, row=3, padx=4, pady=4)
+        self.entryEdad=ttk.Entry(self.labelframe1, textvariable=self.Edad)
+        self.entryEdad.grid(column=1, row=3, padx=4, pady=4)
+        
+        # self.cboPais = ttk.Combobox(self.labelframe1,state="readonly")
+        # self.cboPais.grid(column=1, row=4, padx=4, pady=4)
+        #
+        self.boton1=ttk.Button(self.labelframe1, text="Actualizar", command=self.Actualizar)
+        self.boton1.grid(column=1, row=4, padx=4, pady=4)
+
+    def Actualizar(self):
+        Datos = (self.Nombre.get(), self.Apellido.get(), self.Edad.get(),self.Cedula.get())
+        cantidad=self.registrar.Actualizar(Datos)
+        if cantidad==1:
+            mb.showinfo("Información", "Se modificó el usuario")
+        else:
+            mb.showinfo("Información", "No existe ese usuario")   
+
+    
 
 aplicacion1=Aplicacion()
